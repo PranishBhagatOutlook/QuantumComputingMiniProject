01f161cd2b2e4aa852dd8b8bc67103255b451565
Pranish Bhagat <pranish_bhagat1@baylor.edu>
2021-08-08 08:22:28 -0500

    added details

--- src/Components/About.js
+++ src/Components/About.js
@@ -4,8 +4,34 @@ import "../App.css";
 function About() {
   return (
     <div className="about">
-      <h1>ABOUT</h1>
-      <p></p>
+      <h1>Introduction to quantum computing</h1>
+      <p>
+        To fully define quantum computing, we need to define some key terms
+        first
+      </p>
+      <h2>What is quantum?</h2>
+      <p>
+        The quantum in "quantum computing" refers to the quantum mechanics that
+        the system uses to calculate outputs. In physics, a quantum is the
+        smallest possible discrete unit of any physical property. It usually
+        refers to properties of atomic or subatomic particles, such as
+        electrons, neutrinos, and photons.
+      </p>
+      <h2>What is a qubit?</h2>
+      <p>
+        A qubit is the basic unit of information in quantum computing. Qubits
+        play a similar role in quantum computing as bits play in classical
+        computing, but they behave very differently. Classical bits are binary
+        and can hold only a position of 0 or 1, but qubits can hold a
+        superposition of all possible states.
+      </p>
+      <h2>What is quantum computing? </h2>
+      <p>
+        Quantum computers harness the unique behavior of quantum physics—such as
+        superposition, entanglement, and quantum interference—and apply it to
+        computing. This introduces new concepts to traditional programming
+        methods.
+      </p>
     </div>
   );
 }
--- src/Components/Home.js
+++ src/Components/Home.js
@@ -2,12 +2,55 @@ import React from "react";
 import "../App.css";
 
 function Home() {
+  let githubUrl =
+    "https://github.com/PranishBhagatOutlook/QuantumComputingMiniProject";
+  let notebookUrl = "https://github.com/PranishBhagatOutlook/qcNotebook";
   return (
     <div className="Home">
-      <p>
-        This is a simple frontend that is submitted as a part of mini project
-        assignment for Quantum Computing.
-      </p>
+      <h1>My submission for the mini project for Quantum Computing.</h1>
+      <h2 style={{ textAlign: "left" }}>Things done:</h2>
+      <ol style={{ textAlign: "left" }}>
+        <li>
+          Implemented{" "}
+          <a
+            href="https://arxiv.org/abs/quant-ph/9508027"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <b>Shor's Algorithm</b>
+          </a>{" "}
+          using{" "}
+          <a href="https://qiskit.org/" target="_blank" rel="noreferrer">
+            <i>Qiskit</i>
+          </a>{" "}
+          . Jupyter Notebook link attached{" "}
+          <a href={notebookUrl} target="_blank" rel="noreferrer">
+            {" "}
+            here{" "}
+          </a>
+        </li>
+        <li>
+          Developed this website which contains all the details regarding the
+          algorithm. Github link for this site attached{" "}
+          <a href={githubUrl} target="_blank" rel="noreferrer">
+            {" "}
+            here{" "}
+          </a>
+        </li>
+        <li>
+          Prepared a review report on the implementation. Download the report
+          <a
+            href={process.env.PUBLIC_URL + "/assets/files/test.pdf"}
+            target="_blank"
+            download
+            rel="noreferrer"
+          >
+            {" "}
+            here
+          </a>
+        </li>
+      </ol>
+
       <div>
         <img
           className="qc"
